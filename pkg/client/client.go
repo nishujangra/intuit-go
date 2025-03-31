d8882978d9f860b1da7323d2af8ca14ae289beb5
nishujangra <ndjangra1027@gmail.com>
2025-03-31 10:42:42 +0530

    Logger Added

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,15 +18,23 @@ type Client struct {
 	BaseURL     string
 	AccessToken string
 	HTTPClient  *http.Client
+	Logger      *log.Logger
 }
 
 func NewClient(accessToken string) *Client {
+
+	logFile, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &Client{
 		BaseURL:     config.GetPaymentsBaseURL(),
 		AccessToken: accessToken,
 		HTTPClient: &http.Client{
 			Timeout: 10 * time.Second,
 		},
+		Logger: log.New(logFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
 	}
 }
 
@@ -47,10 +57,21 @@ func (c *Client) requestHelper(method, endpoint string, body interface{}) (*http
 
 	req.Header.Set("Authorization", "Bearer "+c.AccessToken)
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	requestID := uuid.New().String()
 
 	if method == "POST" {
-		req.Header.Set("request-id", uuid.New().String())
+		req.Header.Set("request-id", requestID)
 	}
+
+	c.Logger.Printf(
+		"Response Status: %s, Request ID: %s, URL: %s, Method: %s",
+		"Requesting",
+		requestID,
+		url,
+		method,
+	)
 
 	return c.HTTPClient.Do(req)
 }
